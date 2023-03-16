36774c618aabe7d37a1596d5a4d03fcfde7de7af
Lesha is coming <svcrifice@yandex.ru>
2023-03-16 20:11:39 +0700

    set

--- CombustionEngine/Model/Engine.cs
+++ CombustionEngine/Model/Engine.cs
@@ -2,9 +2,9 @@
     public abstract class Engine {
         public abstract float[] RotationSpeed { get; set; }
         public abstract float[] Torque { get; set; }
-        public abstract float EngineTemp { get; set; }
+        public abstract float EngineTemp { get; protected set; }
         public abstract float TimeElapsed { get; set; }
-        public abstract float AmbientTemp { get; set; }
+        public abstract float AmbientTemp { get; protected set; }
 
         protected abstract float GetTorque(float[] rotationSpeed, float[] torque, float point);
 
